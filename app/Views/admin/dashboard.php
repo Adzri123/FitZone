646020ea2c45d260d52c3dd94b442f28b67a2924
Roy <muhammadadzri11@gmail.com>
2025-07-03 03:35:05 +0800

    up

--- app/Views/admin/dashboard.php
+++ app/Views/admin/dashboard.php
@@ -4,6 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <title>Fitzone Admin Dashboard</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.tailwindcss.com"></script>
     <!-- Chart.js CDN MUST come before your JS -->
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
